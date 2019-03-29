40a60ffeb6212a33622cb2d933830f76afebadbc
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2019-03-29 11:42:51 -0400

    define functions

---
+++ main.py
@@ -0,0 +1,37 @@
+import sys
+
+
+class Operation:
+    def __init__(self, operation, numbers):
+        self.operation = operation
+        self.numbers = numbers
+
+def introduction():
+    print("Python Calculator")
+
+def print_instructions():
+    instructions = "Type your command in this format: "
+    instructions += " add 1, 2"
+    instructions += " Possible operations: add, subtract, multiply, divide"
+    print(instructions)
+
+def get_user_input():
+    pass
+
+def parse_input(string):
+    pass
+
+def run_operation(operation):
+    pass
+
+def answer_user(answer):
+    pass
+
+def ask_again():
+    pass
+
+def main():
+    pass
+
+#main()
+
